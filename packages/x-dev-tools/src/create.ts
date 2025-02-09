1bb4edd32936df2858a16d0ce74b7eefa2e1b248
suk.bear <suk.bear.suwu@gmail.com>
2025-02-09 11:13:29 +0800

    chore: add nuxt template

--- packages/x-dev-tools/src/create.ts
+++ packages/x-dev-tools/src/create.ts
@@ -11,14 +11,17 @@ export async function createProject(targetDir: string, template: string) {
   const cwd = process.cwd()
   const projectPath = path.resolve(cwd, targetDir)
 
-  // 目前只支持 vite 模板
-  if (template !== 'vite') {
+  const VALID_TEMPLATES = ['vite', 'nuxt']
+
+  // 支持的模板类型
+  if (!VALID_TEMPLATES.includes(template)) {
     consola.warn(`暂不支持 ${template} 模板，将使用默认的 vite 模板`)
+    template = 'vite'
   }
 
   try {
     // 获取模板路径
-    const templatePath = path.resolve(__dirname, '../templates/vite')
+    const templatePath = path.resolve(__dirname, `../templates/${template}`)
 
     // 复制模板到目标目录
     await fs.copy(templatePath, projectPath)
